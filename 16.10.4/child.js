4f0ff00c17dfbb1eff85d1b762f485a83de823e5
“Austin” <30918019+haywang@users.noreply.github.com>
2025-06-09 10:01:59 +0800

    add fork

---
+++ 16.10.4/child.js
@@ -0,0 +1,3 @@
+process.on("message", (message) => {
+  process.send({ hypotenuse: Math.hypot(message.x, message.y) });
+});
---
+++ 16.10.4/fork.js
@@ -0,0 +1,9 @@
+const child_process = require("child_process");
+let child = child_process.fork(`${__dirname}/child.js`);
+
+child.send({ x: 4, y: 3 });
+
+child.on("message", (message) => {
+  console.log(message.hypotenuse);
+  child.disconnect();
+});
